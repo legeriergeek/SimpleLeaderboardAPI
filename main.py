4cc131e3d751cfa9c0e47b1bed4cf5cbc1a7cb20
legeriergeek <102820839+legeriergeek@users.noreply.github.com>
2025-03-20 20:11:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ with open("data.json", "r") as f:
 @hug.get('/add_player')
 def add_player(username, score:hug.types.number=1):
     """Adds player to the 'db'"""
+    if user for user in data["users"] if user["username"] == username:
+        return("User already exists! Please retry with a valid username.")
     new_user = {"username": username, "score": score}
     data["users"].append(new_user)
     with open("data.json", "w") as f:
@@ -15,48 +17,23 @@ def add_player(username, score:hug.types.number=1):
 
 @hug.get('/get_users')
 def get_users():
-    """Gets all users in the 'db'"""
     return(data)
 
 @hug.get('/get_user_info')
 def get_user_info(username):
-    """Gets user info based on the username"""
     user_info = next((user for user in data["users"] if user["username"] == username), None)
     return(user_info)
 
 @hug.get('/get_player_score')
 def get_player_score(username):
-    """Gets specified player's score"""
     user_info = next((user for user in data["users"] if user["username"] == username), None)
     return(user_info["score"])
-
-@hug.get('/get_player_score')
-def get_player_score(username):
-    """Gets specified player's score"""
-    user_info = next((user for user in data["users"] if user["username"] == username), None)
-    return(user_info["score"])
-    
-
-@hug.get('/update_player_score')
-def update_player_score(username, score:hug.types.number=1):
-    """Gets specified player's score"""
-    user_info = next((user for user in data["users"] if user["username"] == username), None)
-    user_info["score"] = score
-    with open("data.json", "w") as f:
-        json.dump(data, f, indent=4)
-    return("Score modified sucessfully!")
 
 
 @hug.get('/remove_user')
 def remove_user(username):
-    """Removes specified user"""
     user_info = next((user for user in data["users"] if user["username"] == username), None)
     data["users"].remove(user_info)
     with open("data.json", "w") as f:
         json.dump(data, f, indent=4)
     return("User sucessfully removed!")
-
-
-
-
-
